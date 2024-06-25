16156d712996ad7f372da55afd5fa6ae5d36187e
busekeklik <busekeklik23@gmail.com>
2024-06-25 20:53:13 +0300

    getProducts is working

--- frontend/src/pages/Product.js
+++ frontend/src/pages/Product.js
@@ -5,14 +5,16 @@ import './Product.css';
 const Product = ({ product }) => {
   return (
     <div className="product">
-      <Link to={`/${product.category}/${product.id}`}>
+      <Link to={`/${product.category}/${product.id}`} className="product-link">
         <img src={product.image} alt={product.name} />
         <h3>{product.name}</h3>
         <p>${product.price}</p>
       </Link>
-      <Link to={`/${product.category}/${product.id}`} className="add-to-cart-link">
-        Add to Cart
-      </Link>
+      <button className="add-to-cart-button">
+        <Link to={`/${product.category}/${product.id}`} className="add-to-cart-link">
+          Add to Cart
+        </Link>
+      </button>
     </div>
   );
 };
